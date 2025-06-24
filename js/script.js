08adb680f97230906389f0ea5c842306938a20a4
JailtonJR <1jailtonrodrigues@gmail.com>
2025-06-24 19:39:35 -0300

    portifolio

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function toggleMode() { 
+       const html = document.documentElement;
+       html.classList.toggle("light"); 
+     
+       const img = document.querySelector("#profile img"); 
+
+       if (html.classList.contains("light")) { 
+              img.setAttribute("src", "../img/Imagem_Claro.jpeg"); 
+       }    else  {
+              img.setAttribute("src", "../img/Imagem_Escuro.jpeg"); 
+       }
+}
